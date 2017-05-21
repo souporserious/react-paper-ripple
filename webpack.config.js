df10c3583bfd9f580e9bfee71781c6fffcfb2eb6
Travis Arnold <ftntravis@gmail.com>
2017-05-21 13:46:37 -0700

    update and fix dev build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-var path = require('path');
-var nodeModulesDir = path.resolve(__dirname, 'node_modules');
+var path = require('path')
+var nodeModulesDir = path.resolve(__dirname, 'node_modules')
 
 module.exports = {
   entry: {
-    index: ['webpack/hot/dev-server', './example/index.jsx']
+    index: ['webpack/hot/dev-server', './example/index.jsx'],
   },
   output: {
     path: './example',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
-    { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel?stage=0' },
-    { test: /\.scss$/, loader: 'style!css!sass?sourceMap' }
-    ]
+      { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.scss$/, loader: 'style!css!sass?sourceMap' },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   devServer: {
     contentBase: './example',
-  }
-};
+  },
+}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,31 +1,31 @@
-var path = require('path');
-var webpack = require('webpack');
-var TARGET = process.env.TARGET || null;
+var path = require('path')
+var webpack = require('webpack')
+var TARGET = process.env.TARGET || null
 
 const externals = {
-  'react': {
+  react: {
     root: 'React',
     commonjs2: 'react',
     commonjs: 'react',
-    amd: 'react'
+    amd: 'react',
   },
-  'react-dom': {
-    root: 'ReactDOM',
-    commonjs2: 'react-dom',
-    commonjs: 'react-dom',
-    amd: 'react-dom'
+  'prop-types': {
+    root: 'PropTypes',
+    commonjs2: 'prop-types',
+    commonjs: 'prop-types',
+    amd: 'prop-types',
   },
   'react-motion': {
     root: 'ReactMotion',
     commonjs2: 'react-motion',
     commonjs: 'react-motion',
-    amd: 'react-motion'
-  }
-};
+    amd: 'react-motion',
+  },
+}
 
 var config = {
   entry: {
-    index: './src/react-paper-ripple.js'
+    index: './src/react-paper-ripple.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -33,31 +33,33 @@ var config = {
     filename: 'react-paper-ripple.js',
     sourceMapFilename: 'react-paper-ripple.sourcemap.js',
     library: 'ReactPaperRipple',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
     loaders: [
-    { test: /\.(js|jsx)/, loader: 'babel?stage=0' }
-    ]
+      { test: /\.(js|jsx)/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
   },
   plugins: [],
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   externals: externals,
-};
-
-if (TARGET === 'minify') {
-  config.output.filename = 'react-paper-ripple.min.js';
-  config.output.sourceMapFilename = 'react-paper-ripple.min.js';
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    },
-    mangle: {
-      except: ['React', 'ReactDOM', 'ReactMotion', 'ReactPaperRipple']
-    }
-  }));
 }
 
-module.exports = config;
+if (TARGET === 'minify') {
+  config.output.filename = 'react-paper-ripple.min.js'
+  config.output.sourceMapFilename = 'react-paper-ripple.min.js'
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      mangle: {
+        except: ['React', 'ReactDOM', 'ReactMotion', 'ReactPaperRipple'],
+      },
+    })
+  )
+}
+
+module.exports = config
